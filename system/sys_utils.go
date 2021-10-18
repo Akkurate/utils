8e6ebf72a6cf32430f1f95aa298074eff38d4f80
Jani <jani.maki@akkurate.fi>
2021-10-18 09:19:02 +0300

    fix: change sys to system

--- system/sys_utils.go
+++ system/sys_utils.go
@@ -1,17 +1,17 @@
-package sys
+package system
 
 import (
 	"os"
 	"path/filepath"
 	"runtime"
 
-	"github.com/Akkurate/utils/num"
+	"github.com/Akkurate/utils/numf"
 )
 
 func GetSysmemory() float64 {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
-	return num.RoundTo(float64(m.Sys)/1024/1024, 5)
+	return numf.RoundTo(float64(m.Sys)/1024/1024, 5)
 }
 
 // FileExists FileExists
