262d4a4cb51794d52e23dbb47207f65f8606807c
nchanged <nchanged@gmail.com>
2021-10-27 10:55:37 +0300

    v0.8.0

---
+++ misc/timeout.go
@@ -0,0 +1,22 @@
+package misc
+
+import "time"
+
+type Timeout struct {
+	Released bool
+	Occured  bool
+}
+
+func (t *Timeout) Run(seconds int, callback func()) {
+	go func() {
+		time.Sleep(time.Duration(seconds) * time.Second)
+		if !t.Released {
+			t.Occured = true
+			callback()
+		}
+	}()
+}
+
+func (t *Timeout) Release() {
+	t.Released = true
+}
