fc746bee3793ed97572c6e6adb73bc4faf7dfc87
Jani <jani.maki@akkurate.fi>
2022-03-15 08:10:39 +0200

    feat: Inplace functions

--- numf/nan.go
+++ numf/nan.go
@@ -161,3 +161,103 @@ func FillNan(slice []float64, method string, prefill bool, validTime int) []floa
 
 	return res
 }
+
+// Fills NaN values with a value based on given method:
+//  "previous" // fills the NaNs with previous value
+//  "linear"   // fills the NaNs with linear interpolation
+// Filling starts from first valid value, thus leaving any preceding NaNs untouched. By setting prefill = true, first valid value is used to replace also the preceding NaNs.
+//
+// One sample's lifetime can be set by setting validTime value to > 0. Filling is then performed for validTime samples. Using validTime overrides prefill = true -setting
+func FillNanInplace(slice *[]float64, method string, prefill bool, validTime int) {
+	if len(*slice) == 0 {
+		return
+	}
+	method = strings.ToLower(method)
+
+	firstvalue := (*slice)[0]
+
+	// find first non-NaN value; exit if none found
+	firstvalidvalue := math.NaN()
+	for _, v := range *slice {
+		if IsValid(v) {
+			firstvalidvalue = v
+			break
+		}
+	}
+	if !IsValid(firstvalidvalue) {
+		return
+	}
+
+	validTime = numi.Select(validTime == 0, len(*slice), validTime) // validtime=0 --> fill all the way
+	vt := -1
+
+	if prefill {
+		firstvalue = firstvalidvalue // prefill sets the first valid value as first sample
+	}
+
+	switch method {
+
+	case "previous":
+
+		for i, v := range *slice {
+			if math.IsNaN(v) {
+				vt++
+				if i == 0 {
+					(*slice)[i] = firstvalue
+				} else if vt >= validTime {
+					(*slice)[i] = v
+				} else {
+					(*slice)[i] = (*slice)[i-1]
+				}
+			} else {
+				(*slice)[i] = v
+				vt = 0
+			}
+		}
+
+	case "linear":
+
+		var idx []int
+		var val []float64
+
+		(*slice)[0] = firstvalue // prefill ...
+
+		// find indexes and values of each valid value
+		for i, v := range *slice {
+			if !math.IsNaN(v) {
+				idx = append(idx, i)
+				val = append(val, v)
+			}
+		}
+		// add enpoint
+		idx = append(idx, len(*slice))
+		val = append(val, val[len(val)-1])
+	
+		for i := 0; i < len(val)-1; i++ {
+			vt = 0
+			gapsize := float64(idx[i+1] - idx[i])
+			gapdelta := val[i+1] - val[i]
+
+			if gapsize > 1 {
+				linstep := gapdelta / gapsize
+				for j := idx[i]; j < idx[i+1]; j++ {
+					vt++
+					if vt >= validTime {
+						if j < len(*slice)-1 {
+							(*slice)[j+1] = math.NaN()
+						}
+					} else {
+						if j < len(*slice)-1 {
+							(*slice)[j+1] = (*slice)[j] + linstep
+						}
+					}
+				}
+			}
+		}
+
+	default:
+		return
+	}
+
+	return
+}
